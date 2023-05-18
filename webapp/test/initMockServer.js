6a03123dfeaeac9911797a9989a73d294d29476b
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2023-05-18 13:55:07 -0300

    fix: step 26

--- webapp/test/initMockServer.js
+++ webapp/test/initMockServer.js
@@ -1,11 +1,11 @@
 sap.ui.define([
-    "../localService/mockserver"
+	"../localService/mockserver"
 ], function (mockserver) {
-    "use strict";
+	"use strict";
 
-    // initialize the mock server
-    mockserver.init();
+	// initialize the mock server
+	mockserver.init();
 
-    // initialize the embedded component on the HTML page
-    sap.ui.require(["sap/ui/core/ComponentSupport"]);
+	// initialize the embedded component on the HTML page
+	sap.ui.require(["sap/ui/core/ComponentSupport"]);
 });
